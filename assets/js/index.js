fa05c573a2a0f69e7d5ddd3a0dbda895d7c58501
aguerreroperez1973 <aguerreroperez@hotmail.com>
2023-08-27 12:41:47 -0400

    subir primera version

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+ precio = 400000;
+        cantidad = 0;
+      
+        precioSpan = document.querySelector(".precio-inicial");
+        precioSpan.innerHTML = precio;
+
+        valorTotal = document.querySelector('.valor-total');
+        valorTotal.innerHTML = 0;
+
+        cantidadSpan = document.querySelector(".cantidad");
+        cantidadSpan.innerHTML = cantidad;  
